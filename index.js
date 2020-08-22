4ce733cced9cb2dfef942e7ad558ca52b05449e7
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-22 17:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function deepestChild() {
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
       const firstList = rankedLists[0]
-      const secondList = rankedLists[1]
+      const secondList = rankedLists[1]let children = firstList.children
+      let start = 1
 for (let i = 0, l = children.length; i < l; i++) {
         expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
       }
-      let children = firstList.children
-      let start = 1
+      
 
 }
