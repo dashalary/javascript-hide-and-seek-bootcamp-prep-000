affe0a0177b81365d0398851a2b1199e3c79937e
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-22 17:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
       const firstList = rankedLists[0]
       const secondList = rankedLists[1]
-
+for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
+      }
       let children = firstList.children
       let start = 1
 
